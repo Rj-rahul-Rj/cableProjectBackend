2bc27bcb01b896479d6ee7f6e89faa30a1f9be36
unknown <rahul.joshi@mmfinfotech.in>
2023-06-26 12:55:46 +0530

    chang

--- util/service.js
+++ util/service.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
 exports.conn = mysql.createConnection({
-    host:'localhost',
+    host:'127.0.0.1',
     user:'root',
     password:'',
-    database:'cable'
+    database:'cable',
+    path: '/'
 });
 
 exports.conn.connect((err)=>{
